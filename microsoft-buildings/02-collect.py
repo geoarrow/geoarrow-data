21ef4e889a2448c76106a692d2ca43e9361e6c3c
Dewey Dunnington <dewey@fishandwhistle.net>
2023-04-24 23:03:31 -0300

    format

--- microsoft-buildings/02-collect.py
+++ microsoft-buildings/02-collect.py
@@ -1,17 +1,16 @@
-
 import pyarrow as pa
 import pyarrow.parquet as parquet
 import geoarrow.pyarrow as ga
 
-zip_codes_from_ogr = parquet.read_table('us-zip-codes/us-zip-codes-ogr.parquet')
+zip_codes_from_ogr = parquet.read_table("us-zip-codes/us-zip-codes-ogr.parquet")
 
 crs84_json = None
-with open('ogc_crs84.json') as f:
+with open("ogc_crs84.json") as f:
     crs84_json = f.read().strip()
 
 geom_type = ga.wkb().with_crs(crs84_json, ga.CrsType.PROJJSON)
 geom = pa.chunked_array(
-    [geom_type.wrap_array(a) for a in zip_codes_from_ogr.column('geometry').chunks]
+    [geom_type.wrap_array(a) for a in zip_codes_from_ogr.column("geometry").chunks]
 )
 
 geo_meta_json = """
@@ -28,24 +27,21 @@ geo_meta_json = """
 """
 
 zip_codes_wkb = pa.table(
-    [zip_codes_from_ogr.column('NAME20'), geom],
+    [zip_codes_from_ogr.column("NAME20"), geom],
     schema=pa.schema(
-        [
-            pa.field('zip_code', pa.utf8()),
-            pa.field('geometry', geom_type)
-        ]
-    ).with_metadata({'geo': geo_meta_json})
+        [pa.field("zip_code", pa.utf8()), pa.field("geometry", geom_type)]
+    ).with_metadata({"geo": geo_meta_json}),
 )
 
 parquet.write_table(
-    zip_codes_wkb,
-    'us-zip-codes/us-zip-codes-wkb.parquet',
-    compression='ZSTD'
+    zip_codes_wkb, "us-zip-codes/us-zip-codes-wkb.parquet", compression="ZSTD"
 )
 
-geom_type_interleaved = ga.multipolygon() \
-    .with_coord_type(ga.CoordType.INTERLEAVED) \
+geom_type_interleaved = (
+    ga.multipolygon()
+    .with_coord_type(ga.CoordType.INTERLEAVED)
     .with_crs(crs84_json, ga.CrsType.PROJJSON)
+)
 
 geom_interleaved = pa.chunked_array(
     [chunk.as_geoarrow(geom_type_interleaved) for chunk in geom.chunks]
@@ -65,40 +61,33 @@ geo_meta_json = """
 """
 
 zip_codes_interleaved = pa.table(
-    [zip_codes_from_ogr.column('NAME20'), geom_interleaved],
+    [zip_codes_from_ogr.column("NAME20"), geom_interleaved],
     schema=pa.schema(
-        [
-            pa.field('zip_code', pa.utf8()),
-            pa.field('geometry', geom_type_interleaved)
-        ]
-    ).with_metadata({'geo': geo_meta_json})
+        [pa.field("zip_code", pa.utf8()), pa.field("geometry", geom_type_interleaved)]
+    ).with_metadata({"geo": geo_meta_json}),
 )
 
 parquet.write_table(
     zip_codes_interleaved,
-    'us-zip-codes/us-zip-codes-interleaved-multipolygon.parquet',
-    compression='ZSTD'
+    "us-zip-codes/us-zip-codes-interleaved-multipolygon.parquet",
+    compression="ZSTD",
 )
 
-geom_type_geoarrow = ga.multipolygon() \
-    .with_crs(crs84_json, ga.CrsType.PROJJSON)
+geom_type_geoarrow = ga.multipolygon().with_crs(crs84_json, ga.CrsType.PROJJSON)
 
 geom_geoarrow = pa.chunked_array(
     [chunk.as_geoarrow(geom_type_geoarrow) for chunk in geom.chunks]
 )
 
 zip_codes_geoarrow = pa.table(
-    [zip_codes_from_ogr.column('NAME20'), geom_geoarrow],
+    [zip_codes_from_ogr.column("NAME20"), geom_geoarrow],
     schema=pa.schema(
-        [
-            pa.field('zip_code', pa.utf8()),
-            pa.field('geometry', geom_type_geoarrow)
-        ]
-    ).with_metadata({'geo': geo_meta_json})
+        [pa.field("zip_code", pa.utf8()), pa.field("geometry", geom_type_geoarrow)]
+    ).with_metadata({"geo": geo_meta_json}),
 )
 
 parquet.write_table(
     zip_codes_geoarrow,
-    'us-zip-codes/us-zip-codes-multipolygon.parquet',
-    compression='ZSTD'
+    "us-zip-codes/us-zip-codes-multipolygon.parquet",
+    compression="ZSTD",
 )
