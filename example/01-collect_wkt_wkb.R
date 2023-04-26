40fb5f3fe5c84291249ffc6fa481955185568407
Dewey Dunnington <dewey@fishandwhistle.net>
2023-04-25 22:10:26 -0300

    better original parquets

--- example/01-collect_wkt_wkb.R
+++ example/01-collect_wkt_wkb.R
@@ -33,10 +33,22 @@ wkt_schema$children$geometry$metadata[["ARROW:extension:name"]] <-
   "geoarrow.wkt"
 wkt_schema <- as_schema(wkt_schema)
 
-geo_metadata_template <- '{
+geo_metadata_template_wkb <- '{
   "columns": {
     "geometry": {
       "encoding": "WKB",
+      "crs": null,
+      "geometry_types": ["%s"]
+    }
+  },
+  "primary_column": "geometry",
+  "version": "1.0.0-dev"
+}'
+
+geo_metadata_template_wkt <- '{
+  "columns": {
+    "geometry": {
+      "encoding": "geoarrow.wkt",
       "crs": null,
       "geometry_types": ["%s"]
     }
@@ -52,14 +64,33 @@ geometry_type_map <- c(
 )
 
 for (nm in names(examples_wk_wkt)) {
-  geometry_type <- geometry_type_map[wk::wk_meta(examples_wk_wkt[[nm]])$geometry_type[1]]
+  meta0 <- wk::wk_meta(examples_wk_wkt[[nm]])[1, , drop = FALSE]
+  dims <- if (meta0$has_z && meta0$has_m) {
+    " ZM"
+  } else if (meta0$has_z) {
+    " Z"
+  } else if (meta0$has_m) {
+    " M"
+  } else {
+    ""
+  }
 
-  wkt_schema$metadata$geo <- sprintf(geo_metadata_template, geometry_type)
-  wkb_schema$metadata$geo <- sprintf(geo_metadata_template, geometry_type)
+  geometry_type <- paste0(geometry_type_map[meta0$geometry_type], dims)
+
+  wkt_schema$metadata$geo <- sprintf(geo_metadata_template_wkt, geometry_type)
+  wkb_schema$metadata$geo <- sprintf(geo_metadata_template_wkb, geometry_type)
 
   table_wkt <- Table$create(geometry = examples_wkt[[nm]], schema = wkt_schema)
   table_wkb <- Table$create(geometry = examples_wkb[[nm]], schema = wkb_schema)
 
-  write_parquet(table_wkt, glue::glue("example/{nm}-wkt.parquet"), compression = "uncompressed")
-  write_parquet(table_wkb, glue::glue("example/{nm}-wkb.parquet"),  compression = "uncompressed")
+  write_parquet(
+    table_wkt,
+    glue::glue("example/{nm}-wkt.parquet"),
+    compression = "uncompressed"
+  )
+  write_parquet(
+    table_wkb,
+    glue::glue("example/{nm}-wkb.parquet"),
+    compression = "uncompressed"
+  )
 }
---
+++ example/02-collect_geoarrow.py
@@ -0,0 +1,60 @@
+import pyarrow as pa
+import pyarrow.parquet as parquet
+import geoarrow.pyarrow as ga
+import glob
+import os
+import re
+
+for f in glob.glob('example/*-wkt.parquet'):
+
+    table = parquet.read_table(f)
+    geom = table.column('geometry')
+    geo_meta = table.schema.metadata[b'geo'].decode('UTF-8')
+
+    type_out = None
+    geometry_type_str = os.path.basename(f)
+    if geometry_type_str.startswith('point'):
+        type_out = ga.point()
+    elif geometry_type_str.startswith('linestring'):
+        type_out = ga.linestring()
+    elif geometry_type_str.startswith('polygon'):
+        type_out = ga.polygon()
+    elif geometry_type_str.startswith('multipoint'):
+        type_out = ga.multipoint()
+    elif geometry_type_str.startswith('multilinestring'):
+        type_out = ga.multilinestring()
+    elif geometry_type_str.startswith('multipolygon'):
+        type_out = ga.multipolygon()
+    else:
+        # e.g., geometrycollection
+        continue
+
+    geo_meta_out = re.sub('geoarrow.wkt', type_out.extension_name, geo_meta)
+    schema_out = pa.schema([pa.field('geometry', type_out)]) \
+        .with_metadata({'geo': geo_meta_out})
+
+    geom_out = pa.chunked_array(
+        [array.as_geoarrow(type_out) for array in geom.chunks]
+    )
+
+    table_out = pa.table([geom_out], schema=schema_out)
+    parquet.write_table(
+        table_out,
+        re.sub('-wkt.parquet', '.parquet', f),
+        compression='uncompressed'
+    )
+
+    type_out = type_out.with_coord_type(ga.CoordType.INTERLEAVED)
+    schema_out = pa.schema([pa.field('geometry', type_out)]) \
+        .with_metadata({'geo': geo_meta_out})
+
+    geom_out = pa.chunked_array(
+        [array.as_geoarrow(type_out) for array in geom.chunks]
+    )
+
+    table_out = pa.table([geom_out], schema=schema_out)
+    parquet.write_table(
+        table_out,
+        re.sub('-wkt.parquet', '-interleaved.parquet', f),
+        compression='uncompressed'
+    )
