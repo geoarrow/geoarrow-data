6254b6f3c65bb5934b84cee7a87bf943ed38d642
Dewey Dunnington <dewey@fishandwhistle.net>
2023-04-26 23:59:20 -0300

    more renders

--- ns-water/01-collect_gpkg.sh
+++ ns-water/01-collect_gpkg.sh
@@ -2,6 +2,6 @@
 for f in $(find ns-water/nshn_v2 -name '*.shp')
 do
     f_out=$(echo "${f}" | sed -e 's/.shp/.gpkg/' -e 's/nshn_v2_/ns-water_/' -e 's/_ba_/_basin_/' -e 's/_la_/_land_/' -e 's/_wa_/_water_/')
-    ogr2ogr "${f_out}" "${f}" -nlt PROMOTE_TO_MULTI
+    ogr2ogr "${f_out}" "${f}" -dim XY
     mv "${f_out}" ns-water
 done
