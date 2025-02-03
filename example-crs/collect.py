67057ab84311c7139e79744272e0a85a29ab3648
Dewey Dunnington <dewey@wherobots.com>
2025-02-03 16:58:14 -0600

    a few more sources

--- example-crs/collect.py
+++ example-crs/collect.py
@@ -1,13 +1,25 @@
+import zipfile
 from pathlib import Path
 
+import geoarrow.pyarrow as ga
 import geopandas
-
+import pyarrow as pa
+from geoarrow.pyarrow import io
+from geoarrow.rust.io import write_flatgeobuf
+from pyarrow import ipc
 
 # Original source:
 # https://www.naturalearthdata.com/
 
 
 here = Path(__file__).parent
+CRSES = [
+    "OGC:CRS84",
+    "EPSG:4326",
+    "EPSG:32618",
+    "+proj=ortho +lat_0=43.88 +lon_0=-72.69 +ellps=WGS84",
+]
+CRS_LABELS = ["crs84", "4326", "utm", "custom"]
 
 
 def download_vermont():
@@ -15,13 +27,13 @@ def download_vermont():
         "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_1_states_provinces.zip"
     )
 
-    pd = pd[["geometry"]][pd["name"] == "Vermont"]
+    return pd[["geometry"]][pd["name"] == "Vermont"]
 
 
-def write_parquet_crs84(crs_list, label_list, lazy=True):
+def write_parquet(lazy=True):
     pd = None
 
-    for crs, label in zip(crs_list, label_list):
+    for crs, label in zip(CRSES, CRS_LABELS):
         out = here / "files" / f"example-crs_vermont-{label}.parquet"
         if lazy and out.exists():
             continue
@@ -34,13 +46,38 @@ def write_parquet_crs84(crs_list, label_list, lazy=True):
     return out
 
 
+def write_geoarrow(lazy=True):
+    for label in CRS_LABELS:
+        out = here / "files" / f"example-crs_vermont-{label}-wkb.arrows"
+        if lazy and out.exists():
+            continue
+
+        tab = io.read_geoparquet_table(
+            here / "files" / f"example-crs_vermont-{label}.parquet"
+        )
+        with ipc.new_stream(out, tab.schema) as writer:
+            writer.write_table(tab)
+
+
+def write_fgb(lazy=True):
+    for label in CRS_LABELS:
+        out = here / "files" / f"example-crs_vermont-{label}-wkb.fgb.zip"
+        if lazy and out.exists():
+            continue
+
+        tab = io.read_geoparquet_table(
+            here / "files" / f"example-crs_vermont-{label}.parquet"
+        )
+
+        # geoarrow-rust needs "native" and not WKB-encoding
+        tab = pa.table({"geometry": ga.as_geoarrow(tab["geometry"])})
+
+        with zipfile.ZipFile(out, "w", compression=zipfile.ZIP_DEFLATED) as fzip:
+            with fzip.open(out.name.replace(".zip", ""), "w", force_zip64=True) as f:
+                write_flatgeobuf(tab, f, write_index=False)
+
+
 if __name__ == "__main__":
-    write_parquet_crs84(
-        [
-            "OGC:CRS84",
-            "EPSG:4326",
-            "EPSG:32618",
-            "+proj=ortho +lat_0=43.88 +lon_0=-72.69 +ellps=WGS84",
-        ],
-        ["crs84", "4326", "utm", "custom"],
-    )
+    write_parquet()
+    write_geoarrow()
+    write_fgb()
