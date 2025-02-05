07647596843bf726ec75ed56d4719a6d6cde255d
Dewey Dunnington <dewey@wherobots.com>
2025-02-04 23:28:16 -0600

    move natural earth to repo files, add bounds data

--- natural-earth/collect.py
+++ natural-earth/collect.py
@@ -1,11 +1,13 @@
-from pathlib import Path
 import urllib.request
+from pathlib import Path
 
-import geopandas
 import geoarrow.pyarrow as ga
+import geoarrow.types as gat
+import geopandas
+import pyarrow as pa
 from geoarrow.pyarrow import io
-from pyarrow import ipc
-from pyarrow import csv
+from geoarrow.rust.io import write_flatgeobuf
+from pyarrow import csv, ipc
 
 # Original source:
 # https://www.naturalearthdata.com/
@@ -28,10 +30,36 @@ def write_cities():
     )
 
     tab = io.read_geoparquet_table(here / "files" / "natural-earth_cities.parquet")
+    io.write_geoparquet_table(
+        tab,
+        here / "files" / "natural-earth_cities_native.parquet",
+        compression="none",
+        geometry_encoding=io.geoparquet_encoding_geoarrow(),
+    )
+
     with ipc.new_stream(
         here / "files" / "natural-earth_cities_wkb.arrows", tab.schema
     ) as writer:
         writer.write_table(tab)
+
+    tab_native = tab.set_column(1, "geometry", ga.as_geoarrow(tab["geometry"]))
+    with ipc.new_stream(
+        here / "files" / "natural-earth_cities.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    tab_native = tab.set_column(
+        1,
+        "geometry",
+        ga.as_geoarrow(tab["geometry"], coord_type=ga.CoordType.INTERLEAVED),
+    )
+    with ipc.new_stream(
+        here / "files" / "natural-earth_cities_interleaved.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    with open(here / "files" / "natural-earth_cities.fgb", "wb") as f:
+        write_flatgeobuf(tab_native, f, write_index=False)
 
 
 def write_countries():
@@ -48,10 +76,36 @@ def write_countries():
     )
 
     tab = io.read_geoparquet_table(here / "files" / "natural-earth_countries.parquet")
+    io.write_geoparquet_table(
+        tab,
+        here / "files" / "natural-earth_countries_native.parquet",
+        compression="none",
+        geometry_encoding=io.geoparquet_encoding_geoarrow(),
+    )
+
     with ipc.new_stream(
         here / "files" / "natural-earth_countries_wkb.arrows", tab.schema
     ) as writer:
         writer.write_table(tab)
+
+    tab_native = tab.set_column(2, "geometry", ga.as_geoarrow(tab["geometry"]))
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    tab_native = tab.set_column(
+        2,
+        "geometry",
+        ga.as_geoarrow(tab["geometry"], coord_type=ga.CoordType.INTERLEAVED),
+    )
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries_interleaved.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    with open(here / "files" / "natural-earth_countries.fgb", "wb") as f:
+        write_flatgeobuf(tab_native, f, write_index=False)
 
 
 def write_countries_geography():
@@ -66,7 +120,15 @@ def write_countries_geography():
     tab = tab.set_column(i, "geometry", geom)
 
     io.write_geoparquet_table(
-        tab, here / "files" / "natural-earth_countries-geography.parquet"
+        tab,
+        here / "files" / "natural-earth_countries-geography.parquet",
+        compression="none",
+    )
+    io.write_geoparquet_table(
+        tab,
+        here / "files" / "natural-earth_countries-geography_native.parquet",
+        compression="none",
+        geometry_encoding=io.geoparquet_encoding_geoarrow(),
     )
 
     with ipc.new_stream(
@@ -74,8 +136,106 @@ def write_countries_geography():
     ) as writer:
         writer.write_table(tab)
 
+    tab_native = tab.set_column(2, "geometry", ga.as_geoarrow(tab["geometry"]))
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries-geography.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    tab_native = tab.set_column(
+        2,
+        "geometry",
+        ga.as_geoarrow(tab["geometry"], coord_type=ga.CoordType.INTERLEAVED),
+    )
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries-geography_interleaved.arrows",
+        tab_native.schema,
+    ) as writer:
+        writer.write_table(tab_native)
+
+    with open(here / "files" / "natural-earth_countries-geography.fgb", "wb") as f:
+        write_flatgeobuf(tab_native, f, write_index=False)
+
+
+def write_countries_box():
+    tab = io.read_geoparquet_table(here / "files" / "natural-earth_countries.parquet")
+
+    # Manually edit a few of the boxes to have max->min behaviour
+    boxes = ga.box(tab["geometry"])
+    boxes_wkt = ga.as_wkt(boxes).to_pylist()
+    py_boxes = boxes.to_pylist()
+    for i, name in enumerate(tab["name"].to_pylist()):
+        box = py_boxes[i]
+
+        if name == "Fiji":
+            py_boxes[i] = new_box(box, 177.28, -179.79)
+            boxes_wkt[i] = new_box_wkt(py_boxes[i])
+        elif name == "Russia":
+            py_boxes[i] = new_box(box, 19.65, -169.89)
+            boxes_wkt[i] = new_box_wkt(py_boxes[i])
+
+    tab = tab.set_column(
+        2,
+        "geometry",
+        boxes.type.wrap_array(pa.array(py_boxes, boxes.type.storage_type)),
+    )
+    with pa.ipc.new_stream(
+        here / "files" / "natural-earth_countries-bounds_box.arrows", tab.schema
+    ) as writer:
+        writer.write_table(tab)
+
+    tab_wkb = tab.set_column(
+        2, "geometry", ga.with_crs(ga.as_wkb(boxes_wkt), ga.OGC_CRS84)
+    )
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries-bounds_wkb.arrows", tab_wkb.schema
+    ) as writer:
+        writer.write_table(tab_wkb)
+
+    tab_native = tab.set_column(2, "geometry", ga.as_geoarrow(tab_wkb["geometry"]))
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries-bounds.arrows", tab_native.schema
+    ) as writer:
+        writer.write_table(tab_native)
+
+    tab_native = tab.set_column(
+        2,
+        "geometry",
+        ga.as_geoarrow(tab_wkb["geometry"], coord_type=ga.CoordType.INTERLEAVED),
+    )
+    with ipc.new_stream(
+        here / "files" / "natural-earth_countries-bounds_interleaved.arrows",
+        tab_native.schema,
+    ) as writer:
+        writer.write_table(tab_native)
+
+    with open(here / "files" / "natural-earth_countries-bounds.fgb", "wb") as f:
+        write_flatgeobuf(tab_native, f, write_index=False)
+
+
+# Tools to rewrite a box manually specifying the west/east bounds
+def new_box(box, xwest, xeast):
+    return {"xmin": xwest, "ymin": box["ymin"], "xmax": xeast, "ymax": box["ymax"]}
+
+
+def new_box_wkt(b):
+    box_east = {"xmin": b["xmin"], "ymin": b["ymin"], "xmax": 180.0, "ymax": b["ymax"]}
+    box_west = {"xmin": -180.0, "ymin": b["ymin"], "xmax": b["xmax"], "ymax": b["ymax"]}
+    box_east_storage = pa.StructArray.from_arrays(
+        [pa.array([i]) for i in box_east.values()], names=box_east.keys()
+    )
+    box_east_array = gat.box().to_pyarrow().wrap_array(box_east_storage)
+    box_west_storage = pa.StructArray.from_arrays(
+        [pa.array([i]) for i in box_west.values()], names=box_west.keys()
+    )
+    box_west_array = gat.box().to_pyarrow().wrap_array(box_west_storage)
+    box_east_wkt = ga.as_wkt(box_east_array)[0].as_py().replace("POLYGON ", "")
+    box_west_wkt = ga.as_wkt(box_west_array)[0].as_py().replace("POLYGON ", "")
+    return f"MULTIPOLYGON ({box_east_wkt}, {box_west_wkt})"
+
 
 if __name__ == "__main__":
     write_cities()
     write_countries()
     write_countries_geography()
+    write_countries_box()
