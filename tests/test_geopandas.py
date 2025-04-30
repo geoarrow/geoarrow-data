7e893c7ae73ef07c5b3c7e2be4b53da5f12091a5
Dewey Dunnington <dewey@wherobots.com>
2025-04-30 17:56:30 -0500

    add geopandas tests

---
+++ tests/test_geopandas.py
@@ -0,0 +1,77 @@
+import geopandas
+import pyarrow as pa
+import pytest
+
+from . import model
+
+# Skip buildings because it takes too long to load, skip built-in parquet
+# because geopandas doesn't support it
+GEOPANDAS_PARQUET_FILES = [
+    f
+    for f in model.list_files()
+    if "geoparquet" in f.format and "buildings" not in f.group
+]
+
+# Skip buildings because it takes too long to load, skip wkt and box because
+# geopandas doesn't support it, skip anything with m values because it's
+# not supported by geopandas yet
+GEOPANDAS_ARROW_FILES = [
+    f
+    for f in model.list_files()
+    if "arrows" in f.format
+    and "buildings" not in f.group
+    and not f.name.endswith("m")
+    and f.format not in ("arrows/wkt", "arrows/box")
+]
+
+GEOPANDAS_FGB_FILES = [
+    f for f in model.list_files() if "fgb" in f.format and "buildings" not in f.group
+]
+
+
+@pytest.mark.parametrize(
+    "file",
+    GEOPANDAS_PARQUET_FILES,
+    ids=[f.path.name for f in GEOPANDAS_PARQUET_FILES],
+)
+def test_geoparquet(file: model.File):
+    df = geopandas.read_parquet(file.path)
+    assert isinstance(df, geopandas.GeoDataFrame)
+    assert isinstance(df.geometry, geopandas.GeoSeries)
+
+
+@pytest.mark.parametrize(
+    "file",
+    GEOPANDAS_ARROW_FILES,
+    ids=[f.path.name for f in GEOPANDAS_ARROW_FILES],
+)
+def test_from_arrow(file: model.File):
+    if file.format == "arrows/wkt":
+        pytest.skip("geopandas doesn't support geoarrow.wkt")
+
+    with pa.ipc.open_stream(file.path) as reader:
+        tab = reader.read_all()
+    df = geopandas.GeoDataFrame.from_arrow(tab)
+    assert isinstance(df, geopandas.GeoDataFrame)
+    assert isinstance(df.geometry, geopandas.GeoSeries)
+
+
+@pytest.mark.parametrize(
+    "file",
+    GEOPANDAS_FGB_FILES,
+    ids=[f.path.name for f in GEOPANDAS_FGB_FILES],
+)
+def test_fgb(file: model.File):
+    # Check with old engine
+    df = geopandas.read_file(file.path, engine="fiona")
+    assert isinstance(df, geopandas.GeoDataFrame)
+    assert isinstance(df.geometry, geopandas.GeoSeries)
+
+    # Check with and without use_arrow
+    df = geopandas.read_file(file.path, engine="pyogrio", use_arrow=False)
+    assert isinstance(df, geopandas.GeoDataFrame)
+    assert isinstance(df.geometry, geopandas.GeoSeries)
+
+    df = geopandas.read_file(file.path, engine="pyogrio", use_arrow=True)
+    assert isinstance(df, geopandas.GeoDataFrame)
+    assert isinstance(df.geometry, geopandas.GeoSeries)
--- tests/test_structure.py
+++ tests/test_structure.py
@@ -54,8 +54,7 @@ def test_structure(file: model.File):
 def read_format_arrows(file: model.File):
     with ipc.open_stream(file.path) as reader:
         table = reader.read_all()
-        assert "geometry" in table.column_names
-        assert table.column_names[-1] == "geometry"
+        check_field_names(table.column_names)
 
         field = table.schema.field("geometry")
         assert field.metadata[b"ARROW:extension:name"].startswith(b"geoarrow.")
@@ -67,14 +66,15 @@ def read_format_arrows(file: model.File):
 
 def read_format_parquet(file: model.File):
     # Until a released version of pyarrow can actually read these
-    with pytest.raises(OSError, match="Metadata contains Thrift LogicalType that is not recognized"):
+    with pytest.raises(
+        OSError, match="Metadata contains Thrift LogicalType that is not recognized"
+    ):
         parquet.read_table(file.path)
 
 
 def read_format_geoparquet(file: model.File):
     table = parquet.read_table(file.path)
-    assert "geometry" in table.column_names
-    assert table.column_names[-1] == "geometry"
+    check_field_names(table.column_names)
 
     assert b"geo" in table.schema.metadata
 
@@ -157,3 +157,15 @@ def read_format_fgb(file: model.File):
     magic = b"\x66\x67\x62\x03\x66\x67\x62\x00"
     with open(file.path, "rb") as f:
         assert f.read(len(magic)) == magic
+
+
+def check_field_names(field_names):
+    all_names = list(field_names)
+
+    assert "geometry" in field_names
+    assert "geometry" == field_names[-1]
+
+    unique_names = set(all_names)
+    if len(all_names) != len(unique_names):
+        lab = ", ".join("{v}" for v in all_names)
+        raise ValueError(f"Non-unique names: {lab}")
